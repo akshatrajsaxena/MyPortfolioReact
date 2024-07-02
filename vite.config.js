d0ec15c797d2e0b55f53e5d1b7ccc99bb8076460
Akshat Raj Saxena <119042958+akshatrajsaxena@users.noreply.github.com>
2024-07-02 21:43:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/react-portfolio/",
+  base: "/myportfolioreact/",
   plugins: [react()],
   css: {
     modules: {
